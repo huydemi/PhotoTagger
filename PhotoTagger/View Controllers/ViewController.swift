6c43a38cf90d6b3a21185d0d410108a6caeacc2d
Dang Quoc Huy <huysamdua@yahoo.com>
2018-08-21 10:55:20 +0700

    Uploading Files

--- PhotoTagger/View Controllers/ViewController.swift
+++ PhotoTagger/View Controllers/ViewController.swift
@@ -28,6 +28,7 @@
 
 import UIKit
 import SwiftyJSON
+import Alamofire
 
 class ViewController: UIViewController {
 
@@ -95,13 +96,71 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
 
     imageView.image = image
 
+    takePictureButton.isHidden = true
+    progressView.progress = 0.0
+    progressView.isHidden = false
+    activityIndicatorView.startAnimating()
+    
+    upload(image: image,
+           progressCompletion: { [weak self] percent in
+            self?.progressView.setProgress(percent, animated: true)
+    },
+           completion: { [weak self] tags, colors in
+            self?.takePictureButton.isHidden = false
+            self?.progressView.isHidden = true
+            self?.activityIndicatorView.stopAnimating()
+            
+            self?.tags = tags
+            self?.colors = colors
+            
+            self?.performSegue(withIdentifier: "ShowResults", sender: self)
+    })
+    
     dismiss(animated: true)
   }
 }
 
 extension ViewController {
+  
   func upload(image: UIImage,
               progressCompletion: @escaping (_ percent: Float) -> Void,
               completion: @escaping (_ tags: [String]?, _ colors: [PhotoColor]?) -> Void) {
+    guard let imageData = UIImageJPEGRepresentation(image, 0.5) else {
+      print("Could not get JPEG representation of UIImage")
+      return
+    }
+    
+    Alamofire.upload(multipartFormData: { multipartFormData in
+      multipartFormData.append(imageData,
+                               withName: "imagefile",
+                               fileName: "image.jpg",
+                               mimeType: "image/jpeg")
+    },
+                     to: "http://api.imagga.com/v1/content",
+                     headers: ["Authorization": "Basic YWNjXzBlZWE3NzM0MTAzZmJiNzo0ZGJiYmNlZjEyNjRmYmNjYWE1NDcyZThhMzk0ZjcyZA=="],
+                     encodingCompletion: { encodingResult in
+                      switch encodingResult {
+                      case .success(let upload, _, _):
+                        upload.uploadProgress { progress in
+                          progressCompletion(Float(progress.fractionCompleted))
+                        }
+                        upload.validate()
+                        upload.responseJSON { response in
+                          guard response.result.isSuccess,
+                            let value = response.result.value else {
+                              print("Error while uploading file: \(String(describing: response.result.error))")
+                              completion(nil, nil)
+                              return
+                          }
+                          
+                          let firstFileID = JSON(value)["uploaded"][0]["id"].stringValue
+                          print("Content uploaded with ID: \(firstFileID)")
+                          completion(nil, nil)
+                        }
+                      case .failure(let encodingError):
+                        print(encodingError)
+                      }
+    })
   }
+  
 }
